8eca152a8023736380afdda5c7b41a34d3c29457
caio20538 <caio20538@gmail.com>
2025-09-18 16:09:11 -0300

    feat: create product service

---
+++ product-service/src/main/java/com/ecommerce/product_service/service/ProductService.java
@@ -0,0 +1,50 @@
+package com.ecommerce.product_service.service;
+
+import com.ecommerce.product_service.dto.ProductRequest;
+import com.ecommerce.product_service.dto.ProductResponse;
+import com.ecommerce.product_service.model.Product;
+import com.ecommerce.product_service.repository.ProductRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    private static final Logger log = LoggerFactory.getLogger(ProductService.class);
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public void createProduct(ProductRequest productRequest){
+        Product product = new Product.Builder()
+                .name(productRequest.name())
+                .description(productRequest.description())
+                .price(productRequest.price())
+                .build();
+
+        productRepository.save(product);
+
+        log.info("Product {} is saved", product.getId());
+    }
+
+    public List<ProductResponse> getAllProducts() {
+        List<Product> products = productRepository.findAll();
+
+        return products.stream().map(this::mapToProductResponse).toList();
+    }
+
+    private ProductResponse mapToProductResponse(Product product) {
+        return new ProductResponse.Builder()
+                .id(product.getId())
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .build();
+    }
+}
