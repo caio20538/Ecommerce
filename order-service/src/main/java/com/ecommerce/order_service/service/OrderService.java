d257005e3f081b7d64bbb95b45e278e8a3579630
caio20538 <caio20538@gmail.com>
2025-09-18 17:48:48 -0300

    feat: create order service

---
+++ order-service/src/main/java/com/ecommerce/order_service/service/OrderService.java
@@ -0,0 +1,43 @@
+package com.ecommerce.order_service.service;
+
+import com.ecommerce.order_service.dto.OrderLineItemsDto;
+import com.ecommerce.order_service.dto.OrderRequest;
+import com.ecommerce.order_service.model.Order;
+import com.ecommerce.order_service.model.OrderLineItems;
+import com.ecommerce.order_service.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public void placeOrder(OrderRequest orderRequest){
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+
+        List<OrderLineItems> orderLineItems = orderRequest.orderLineItemsDtoList()
+                .stream()
+                .map(this::mapToDto)
+                .toList();
+
+        order.setOrderLineItemsList(orderLineItems);
+
+        orderRepository.save(order);
+    }
+
+    private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
+        OrderLineItems orderLineItems = new OrderLineItems();
+
+        orderLineItems.setPrice(orderLineItemsDto.price());
+        orderLineItems.setQuantity(orderLineItemsDto.quantity());
+        orderLineItems.setSkuCode(orderLineItemsDto.skuCode());
+
+        return orderLineItems;
+    }
+}
