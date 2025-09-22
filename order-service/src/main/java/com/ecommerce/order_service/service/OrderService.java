98abfd0de31802de8e4ff1b305898447c86eb857
caio20538 <caio20538@gmail.com>
2025-09-22 15:58:53 -0300

    fix: double saving orders

--- order-service/src/main/java/com/ecommerce/order_service/service/OrderService.java
+++ order-service/src/main/java/com/ecommerce/order_service/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.ecommerce.order_service.service;
 
+import com.ecommerce.order_service.client.InventoryClient;
 import com.ecommerce.order_service.dto.OrderLineItemsDto;
 import com.ecommerce.order_service.dto.OrderRequest;
 import com.ecommerce.order_service.model.Order;
@@ -8,6 +9,7 @@ import com.ecommerce.order_service.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
 
@@ -16,28 +18,21 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
     public void placeOrder(OrderRequest orderRequest){
+        var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+
+        if (!isProductInStock)
+            throw new RuntimeException("Product with skuCode " + orderRequest.skuCode() + " is not in stock");
+
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
-
-        List<OrderLineItems> orderLineItems = orderRequest.orderLineItemsDtoList()
-                .stream()
-                .map(this::mapToDto)
-                .toList();
-
-        order.setOrderLineItemsList(orderLineItems);
+        order.setPrice(orderRequest.price().multiply(BigDecimal.valueOf(orderRequest.quantity())));
+        order.setSkuCode(orderRequest.skuCode());
+        order.setQuantity(orderRequest.quantity());
 
         orderRepository.save(order);
     }
 
-    private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
-        OrderLineItems orderLineItems = new OrderLineItems();
-
-        orderLineItems.setPrice(orderLineItemsDto.price());
-        orderLineItems.setQuantity(orderLineItemsDto.quantity());
-        orderLineItems.setSkuCode(orderLineItemsDto.skuCode());
-
-        return orderLineItems;
-    }
 }
