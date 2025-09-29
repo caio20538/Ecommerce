2bd5fd76bb81ce104517936f6ae2e229222098fa
caio20538 <caio20538@gmail.com>
2025-09-29 17:18:39 -0300

    feat: implements Resilience4J to project

--- order-service/src/main/java/com/ecommerce/order_service/config/RestClientConfig.java
+++ order-service/src/main/java/com/ecommerce/order_service/config/RestClientConfig.java
@@ -4,9 +4,12 @@ import com.ecommerce.order_service.client.InventoryClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.time.Duration;
 
 @Configuration
 public class RestClientConfig {
@@ -19,13 +22,24 @@ public class RestClientConfig {
     }
 
     @Bean
-    public InventoryClient inventoryClient(){
+    public InventoryClient inventoryClient() {
         RestClient restClient = RestClient.builder()
                 .baseUrl(inventoryServiceUrl)
+                .requestFactory(getClientRequestFactory())
                 .build();
 
         var restClientAdapter = RestClientAdapter.create(restClient);
-        var httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
+        var httpServiceProxyFactory = HttpServiceProxyFactory
+                .builderFor(restClientAdapter)
+                .build();
+
         return httpServiceProxyFactory.createClient(InventoryClient.class);
+    }
+
+    private HttpComponentsClientHttpRequestFactory getClientRequestFactory() {
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+        factory.setConnectTimeout((int) Duration.ofSeconds(3).toMillis());
+        factory.setReadTimeout((int) Duration.ofSeconds(3).toMillis());
+        return factory;
     }
 }
