fc5033856aaf1f4e96c692ffea74f5870ee583cd
caio20538 <caio20538@gmail.com>
2025-10-03 15:01:09 -0300

    fix: fixed error while creating migration

--- inventory-service/src/main/resources/db/migration/V202509191450__create_inventory_table.sql
+++ inventory-service/src/main/resources/db/migration/V202509191450__create_inventory_table.sql
@@ -1,7 +1,6 @@
-CREATE TABLE 't_inventory'
-(
-    id bigint(20) NOT NULL AUTO_INCREMENT,
-    sku_code varchar(255),
-    quantity int (11),
-    PRIMARY KEY ('id')
+CREATE TABLE t_inventory (
+    id BIGINT(20) NOT NULL AUTO_INCREMENT,
+    sku_code VARCHAR(255),
+    quantity INT(11),
+    PRIMARY KEY (id)
 );
