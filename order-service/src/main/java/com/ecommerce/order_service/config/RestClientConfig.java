31fe34aafcc42ebdd30b0ca1748c00121cb9a8fb
caio20538 <caio20538@gmail.com>
2025-10-27 17:42:45 -0300

    feat: create some config

--- order-service/src/main/java/com/ecommerce/order_service/config/RestClientConfig.java
+++ order-service/src/main/java/com/ecommerce/order_service/config/RestClientConfig.java
@@ -1,6 +1,7 @@
 package com.ecommerce.order_service.config;
 
 import com.ecommerce.order_service.client.InventoryClient;
+import io.micrometer.observation.ObservationRegistry;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,11 +18,18 @@ public class RestClientConfig {
     @Value("${inventory.url}")
     private String inventoryServiceUrl;
 
+    private final ObservationRegistry observationRegistry;
+
+    public RestClientConfig(ObservationRegistry observationRegistry) {
+        this.observationRegistry = observationRegistry;
+    }
+
     @Bean
     public InventoryClient inventoryClient() {
         RestClient restClient = RestClient.builder()
                 .baseUrl(inventoryServiceUrl)
                 .requestFactory(getClientRequestFactory())
+                .observationRegistry(observationRegistry)
                 .build();
 
         var restClientAdapter = RestClientAdapter.create(restClient);
